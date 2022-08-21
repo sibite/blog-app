23b36c315799e4e201983a687455eef93e1781b5
sibite <41306389+sibite@users.noreply.github.com>
2022-08-21 16:38:08 +0200

    Configure linting

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App" />
+  );
 }
 
 export default App;
